823c90f1c92a63696e5826fdab147387b1cd18d2
chc1129 <chc1129@gmail.com>
2023-08-20 01:42:30 +0900

    chap04 commit

---
+++ chap04/guess_word/src/lib.rs
@@ -0,0 +1,21 @@
+use rand::seq::SliceRandom;
+use std::collections::HashSet;
+
+const WORDS: &str = include_str!("words.txt");
+
+pub struct Dictionary {
+    words: HashSet<&'static str>,
+}
+
+impl Dictionary {
+    pub fn new() -> Self {
+        let words: HashSet<&str> = WORDS.split("\r\n").collect();
+        Self { words }
+    }
+    pub fn get_random_word(&slef) -> String {
+        Vec::from_iter(self.words.iter())
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .to_string()
+    }
+}
---
+++ chap04/guess_word/src/main.rs
@@ -0,0 +1,5 @@
+use guess_word::Dictionary;
+
+fn main() {
+    println!("{}", Dictioary::new()get_random_word());
+}
