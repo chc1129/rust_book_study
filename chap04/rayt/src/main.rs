3e0f5a06c6d08f65b4f5aaa8bda7c3292d78923d
chc1129 <chc1129@gmail.com>
2023-09-03 18:01:56 +0900

    chap04 commit

--- chap04/rayt/src/main.rs
+++ chap04/rayt/src/main.rs
@@ -200,7 +200,7 @@ impl SimpleScene {
     }
 }
 
-impl Scene for SimpleScene {
+impl SceneWithDepth for SimpleScene {
     fn camera(&self) -> Camera {
         Camera::new(
             Vec3::new(4.0, 0.0, 0.0),
@@ -212,37 +212,30 @@ impl Scene for SimpleScene {
     fn trace(&self, ray: Ray, depth: usize) -> Color {
         let hit_info = self.world.hit(&ray, 0.001, f64::MAX);
         if let Some(hit) = hit_info {
-            let emitted = hit.m.emitted(&ray, &hit);
             let scatter_info = if depth > 0 {
                 hit.m.scatter(&ray, &hit)
             } else {
                 None
             };
             if let Some(scatter) = scatter_info {
-                if let Some(pdf) = scatter.pdf {
-                    let shape_pdf = Arc::new(ShapePdf::new(
-                        Arc::clone(&self.light), hit.p));
-                    let pdf = MixturePdf::new(shape_pdf, Arc::clone(&pdf));
-                    let new_ray = Ray::new(hit.p, pdf.generate(&hit));
-                    let spdf_value = pdf.value(&hit, new_ray.direction);
-                    if spdf_value > 0.0 {
-                        let pdf_value = hit.m.scattering_pdf(&new_ray, &hit);
-                        let albedo = scatter.albedo * pdf_value;
-                        let color = self.trace(new_ray, depth - 1);
-                        emitted + albedo * color / spdf_value
-                    } else {
-                        emitted
-                    }
-                } else {
-                    let color = self.trace(scatter.ray, depth - 1);
-                    emitted + scatter.albedo * color
-                }
+                scatter.albedo * self.trace(scatter.ray, depth - 1)
             } else {
-                emitted
+                Color::zero()
             }
         } else {
             self.background(ray.direction)
         }
+        // match hit_info {
+        //     Some(hit) if depth > 0 => {
+        //         if let Some(scatter) = hit.m.scatter(&ray, &hit) {
+        //             scatter.albedo * self.trace(scatter.ray, depth-1)
+        //         } else {
+        //             Color::zero()
+        //         }
+        //     },
+        //     Some(_) => { Color::zero() },
+        //     None => { self.background_sky(ray.direction) },
+        // }
     }
 }
 
